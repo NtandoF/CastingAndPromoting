a8193c9b7d718b8e365e18c766f4d5d7701758b9
Ntandof <ntandofitshaneversace@gmail.com>
2019-07-17 15:29:51 +0200

    completed the CastingAndPromoting exercise.

---
+++ src/CastingAndPromoting.java
@@ -0,0 +1,20 @@
+public class CastingAndPromoting {
+
+    public static void main(String[] args) {
+
+        long l1 = 123_485_549_152_589_5121L;
+
+        float f1 = 846_265_152_511_516_236_865_752_205f;
+
+        char c1 = 'b';
+
+        System.out.println(l1 + "L");
+
+        int i1 = (int) 123_485_549_152_589_5121L;
+
+        System.out.println(i1);
+
+    }
+
+
+}
